e759557427ca4d4a28b6676a9dac9e3864cdb4d4
Srykah <srykah@gmail.com>
2019-03-31 21:42:23 +0200

    Ajout de plus d'infos sur les paramètres (n,i,j,k)

--- Params.hpp
+++ Params.hpp
@@ -6,10 +6,10 @@
 #define LDPC_PARAMS_HPP
 
 /// @struct Params
-/// @var n nombre de colonnes (largeur)
-/// @var i nombre de 1 par colonne
-/// @var j nombre de 1 par ligne
-/// @var k nombre de lignes (hauteur)
+/// @var n nombre de colonnes (largeur) (nombre de VN)
+/// @var i nombre de 1 par colonne (nombre de CN par VN)
+/// @var j nombre de 1 par ligne (nombre de VN par CN)
+/// @var k nombre de lignes (hauteur) (nombre de CN)
 struct Params {
     Params(int n, int k) : n(n), i(0), j(0), k(k) {}
     Params(int n, int j, int k) : n(n), i(k*j/n), j(j), k(k) {}
